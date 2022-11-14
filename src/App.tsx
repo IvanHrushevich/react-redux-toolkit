7a1e5c762465babfea46b834c8ab99e28bac619c
Ivan Hrushevich <ivan.grushevich1@gmail.com>
2022-11-14 03:12:11 +0300

    fix bug, implement basic working example of using redux-toolkit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { useAppDispatch, useAppSelector } from './hooks/redux';
+import { userSlice } from './store/reducers/UserSlice';
+
 function App() {
-  return <div>Hello</div>;
+  const dispatch = useAppDispatch();
+  const { count } = useAppSelector((state) => state.userReducer);
+  const { increment } = userSlice.actions;
+
+  return (
+    <div>
+      <h2>Count: {count}</h2>
+      <button onClick={() => dispatch(increment(5))}>Increment</button>
+    </div>
+  );
 }
 
 export default App;
